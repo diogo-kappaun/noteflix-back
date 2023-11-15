97c79c2f50a6d792bc0093e8f005e6d29b652fbd
diogo-kappaun <diogohkappaun@gmail.com>
2023-11-15 16:19:27 -0300

    :sparkles: feat: Creating migrations for Users

--- src/database/knex/migration/20231115184933_users.js
+++ src/database/knex/migration/20231115184933_users.js
@@ -1,15 +1,11 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+export const up = (knex) => knex.schema.createTable("users", (table) => {
+  table.increments("id")
+  table.string("name")
+  table.string("email")
+  table.string("password")
+  table.string("avatar").nullable()
+  table.timestamp("created_at").default(knex.fn.now())
+  table.timestamp("updated_at").default(knex.fn.now())
+})
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export const down = (knex) => knex.schema.dropTable("users")
