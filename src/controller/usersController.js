bae72395c0d52446915875fc65f9eb01164ada7c
diogo-kappaun <diogohkappaun@gmail.com>
2023-11-15 17:43:00 -0300

    :sparkles: feat: Creating users

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -1,11 +1,20 @@
 import { AppError } from "../utils/AppError.js";
+import { knexConnection } from "../database/knex/index.js";
+
+const knex = knexConnection
 export class usersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
     if (!name || !email || !password) {
       throw new AppError("All fields are required!");
     }
+
+    await knex("users").insert({
+      name,
+      email,
+      password
+    })
 
     return response.json({ name, email, password });
   }
