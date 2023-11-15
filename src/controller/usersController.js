8f17db017978d09460e9cffa3c6ce6c38fe22acd
diogo-kappaun <diogohkappaun@gmail.com>
2023-11-15 10:59:24 -0300

    :sparkles: feat: Handling request data

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -1,7 +1,12 @@
+import { AppError } from "../utils/AppError.js";
 export class usersController {
   create(request, response) {
     const { name, email, password } = request.body;
 
+    if (!name || !email || !password) {
+      throw new AppError("All fields are required!");
+    }
+    
     return response.json({ name, email, password });
   }
 }
