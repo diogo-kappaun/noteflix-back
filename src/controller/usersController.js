a102ee19891d672cdd551e76cb2a8275a7ef6cc5
diogo-kappaun <diogohkappaun@gmail.com>
2023-11-16 20:09:05 -0300

    :sparkles: feat: Adding update requisition

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -2,8 +2,8 @@ import { AppError } from "../utils/AppError.js";
 import { knexConnection } from "../database/knex/index.js";
 import pkg from "bcryptjs";
 
-const { hash } = pkg
-const knex = knexConnection
+const { hash } = pkg;
+const knex = knexConnection;
 
 export class usersController {
   async create(request, response) {
@@ -13,14 +13,27 @@ export class usersController {
       throw new AppError("All fields are required!");
     }
 
-    const hashPassword = await hash(password, 8)
+    const hashPassword = await hash(password, 8);
 
     await knex("users").insert({
       name,
       email,
       password: hashPassword,
-    })
+    });
 
     return response.json({ name, email, password });
+  }
+
+  async update(request, response) {
+    const { name, email, password } = request.body;
+    const { id } = request.params;
+
+    const user = await knex("users").where({ id });
+
+    if (!user) {
+      throw new AppError("User not found!");
+    }
+
+    return response.json(JSON.stringify(user));
   }
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,5 +6,6 @@ const usersRoutes = Router();
 const userController = new usersController();
 
 usersRoutes.post("/", userController.create);
+usersRoutes.put("/:id", userController.update);
 
 export default usersRoutes;
