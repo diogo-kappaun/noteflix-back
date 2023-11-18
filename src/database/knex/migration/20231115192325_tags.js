90670fe1329e3dac4a49d27ad00425dfa20aee07
diogo-kappaun <diogohkappaun@gmail.com>
2023-11-18 14:56:49 -0300

    :bug: fix: Adding delete cascade

--- src/database/knex/migration/20231115192325_tags.js
+++ src/database/knex/migration/20231115192325_tags.js
@@ -1,9 +1,17 @@
 export const up = (knex) =>
   knex.schema.createTable("tags", (table) => {
     table.increments("id");
-    table.integer("movie_id").references("id").inTable("movies")
-    table.integer("user_id").references("id").inTable("users")
-    table.string("name")
+    table
+      .integer("movie_id")
+      .references("id")
+      .inTable("movies")
+      .onDelete("CASCADE");
+    table
+      .integer("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+    table.string("name");
   });
 
 export const down = (knex) => knex.schema.dropTable("tags");
