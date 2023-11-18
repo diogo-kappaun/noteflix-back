4a65cf2fe73693a60e7474a9bd7dbd9e88379916
diogo-kappaun <diogohkappaun@gmail.com>
2023-11-18 17:23:01 -0300

    :bug: fix: Problem deleting user

--- src/database/knex/migration/20231115192337_movies.js
+++ src/database/knex/migration/20231115192337_movies.js
@@ -4,7 +4,7 @@ export const up = (knex) =>
     table.string("name");
     table.string("description");
     table.integer("rating").unsigned().notNullable().defaultTo(1);
-    table.integer("user_id").references("id").inTable("users");
+    table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
   });
