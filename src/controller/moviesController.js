50fb3b5f7e8052bf527f707c58e1ae0c9fb10b4b
diogo-kappaun <diogohkappaun@gmail.com>
2023-11-19 14:58:42 -0300

    :sparkles: feat: Creating sampling filter

--- src/controller/moviesController.js
+++ src/controller/moviesController.js
@@ -45,22 +45,56 @@ export class moviesController {
   }
 
   async delete(request, response) {
-    const { id } = request.params
+    const { id } = request.params;
 
-    await knex("movies").delete("*").where({ id })
-  
+    await knex("movies").delete("*").where({ id });
+
     return response.json("Movie successfully deleted.");
   }
 
   async show(request, response) {
-    const { id } = request.params
+    const { id } = request.params;
 
-    const note = await knex("movies").where({ id })
-    const tags = await knex("tags").where("movie_id", id).orderBy("name")
+    const note = await knex("movies").where({ id });
+    const tags = await knex("tags").where("movie_id", id).orderBy("name");
 
     return response.json({
       note,
-      tags
-    })
+      tags,
+    });
+  }
+
+  async index(request, response) {
+    const { name, user_id, tags } = request.query;
+
+    let movies;
+
+    if (tags) {
+      const filterTags = tags.split(",").map((tag) => tag.trim());
+
+      movies = await knex("tags")
+        .select(["movies.id", "movies.user_id", "movies.name"])
+        .where("movies.user_id", user_id)
+        .whereLike("movies.name", `%${name}%`)
+        .whereIn("tags.name", filterTags)
+        .innerJoin("movies", "movies.id", "tags.name");
+    } else {
+      movies = await knex("movies")
+        .where({ user_id })
+        .whereLike("name", `%${name}%`);
+    }
+
+    const userTags = await knex("tags").where({ user_id });
+    const moviesWithTags = movies.map((movie) => {
+      const movieTags = userTags.filter((tag) => tag.movie_id === movie.id);
+
+      return {
+        ...movie,
+        tags: movieTags,
+      };
+    });
+
+    return response.json({ moviesWithTags });
   }
 }
+3;
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -5,6 +5,7 @@ const moviesRoutes = Router();
 
 const movieController = new moviesController();
 
+moviesRoutes.get("/", movieController.index)
 moviesRoutes.post("/:user_id", movieController.create);
 moviesRoutes.delete("/:id", movieController.delete)
 moviesRoutes.get("/:id", movieController.show)
