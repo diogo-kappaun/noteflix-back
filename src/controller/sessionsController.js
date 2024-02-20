5c759489a9e20e99f482fe51aac07f9440fb02c7
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-19 21:27:26 -0300

    feat: Add password validation

--- src/controller/sessionsController.js
+++ src/controller/sessionsController.js
@@ -1,5 +1,6 @@
 import { knex } from "../database/knex"
 import { AppError } from "../utils/AppError"
+import { compare } from "bcryptjs"
 
 export class SessionsControllers {
   async create(request, response) {
@@ -8,6 +9,12 @@ export class SessionsControllers {
     const user = await knex('users').where({email}).first()
 
     if (!user) {
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
       throw new AppError("E-mail e/ou senha incorreta", 401)
     }
   }
