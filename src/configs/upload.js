d3bce83aba5c99823df05efa7db880eec7b44704
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-20 20:40:38 -0300

    feat: Create upload configuration

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,1 +1,18 @@
-import { } from 
+import path from "path"
+import Path from "./src/utils/Path.js"
+import multer from "multer"
+
+export const TMP_FOLDER = path.resolve(Path.dirname(import.meta.url), "tmp")
+export const UPLOADS_FOLDER = path.resolve(Path.dirname(import.meta.url), "tmp", "uploads" )
+
+export const MULTER = {
+	storage: multer.diskStorage({
+		destination: TMP_FOLDER,
+		filename(request, file, callback) {
+			const fileHash = crypto.randomBytes(10).toString("hex")
+			const fileName = `${fileHash}-${file.originalname}`
+
+			return callback(null, fileName)
+		}
+	})
+}
