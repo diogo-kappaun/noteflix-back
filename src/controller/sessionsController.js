93d1901122e06e47025b56f14093c34d03e902aa
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-19 21:24:46 -0300

    feat: Add email validation

--- src/controller/sessionsController.js
+++ src/controller/sessionsController.js
@@ -1,5 +1,14 @@
+import { knex } from "../database/knex"
+import { AppError } from "../utils/AppError"
+
 export class SessionsControllers {
   async create(request, response) {
+    const { email, password } = request.body
     
+    const user = await knex('users').where({email}).first()
+
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
   }
 }
