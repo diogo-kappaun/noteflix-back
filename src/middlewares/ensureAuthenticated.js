13b017be084a00960cd18b5ea85525bed8bd3511
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-19 22:22:42 -0300

    feat: Add token authentication middleware

---
+++ src/middlewares/ensureAuthenticated.js
@@ -0,0 +1,26 @@
+import pkg from "jsonwebtoken"
+import { AppError } from "../utils/AppError"
+import authConfig from "../configs/auth"
+const { verify } = pkg
+
+export function ensureAuthenticated(request, response, next) {
+	const authHeader = request.headers.authorization
+
+	if (!authHeader) {
+		throw new AppError("JWT Token inválido", 401)
+	}
+
+	const [, token] = authHeader.split(" ")
+
+	try {
+		const {sub: user_id} = verify(token, authConfig.jwt.secret)
+
+		request.user = {
+			id: Number(user_id)
+		}
+    
+		return next()
+	} catch (error) {
+		throw new AppError("JWT Token inválido", 401)
+	}
+}
