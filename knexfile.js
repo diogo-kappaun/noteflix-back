c855cdfa6dd0b67849201643fb92e042b709b3d9
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-20 20:26:32 -0300

    refactor: Eslint format

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,29 @@
-import Path from "./src/utils/Path.js";
-import path from "path";
+import Path from "./src/utils/Path.js"
+import path from "path"
 
 export default {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: path.resolve(
-        Path.dirname(import.meta.url),
-        "src",
-        "database",
-        "database.db"
-      ),
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: path.resolve(
-        Path.dirname(import.meta.url),
-        "src",
-        "database",
-        "knex",
-        "migration"
-      ),
-    },
-  },
-};
+	development: {
+		client: "sqlite3",
+		connection: {
+			filename: path.resolve(
+				Path.dirname(import.meta.url),
+				"src",
+				"database",
+				"database.db"
+			),
+		},
+		pool: {
+			afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: path.resolve(
+				Path.dirname(import.meta.url),
+				"src",
+				"database",
+				"knex",
+				"migration"
+			),
+		},
+	},
+}
