f7dd633d973b37f42a4ac88a19509e9058f8f991
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-20 19:51:30 -0300

    refactor: Eslint format

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,5 +1,5 @@
-import { Router } from 'express'
-import { SessionsControllers } from '../controller/sessionsController.js'
+import { Router } from "express"
+import { SessionsControllers } from "../controller/sessionsController.js"
 
 const sessionsControllers = new SessionsControllers()
 
