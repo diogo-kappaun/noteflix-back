690d3e99f22e38c3cafa00011b7fac96b6444a9e
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-19 22:14:54 -0300

    refactor: Format code with ESLint

--- src/controller/sessionsController.js
+++ src/controller/sessionsController.js
@@ -9,28 +9,28 @@ const { compare } = pkgb
 const { sign } = pkg
 
 export class SessionsControllers {
-  async create(request, response) {
-    const { email, password } = request.body
+	async create(request, response) {
+		const { email, password } = request.body
     
-    const user = await knex('users').where({email}).first()
+		const user = await knex("users").where({email}).first()
 
-    if (!user) {
-      throw new AppError("E-mail e/ou senha incorreta", 401)
-    }
+		if (!user) {
+			throw new AppError("E-mail e/ou senha incorreta", 401)
+		}
 
-    const passwordMatched = await compare(password, user.password)
+		const passwordMatched = await compare(password, user.password)
 
-    if (!passwordMatched) {
-      throw new AppError("E-mail e/ou senha incorreta", 401)
-    }
+		if (!passwordMatched) {
+			throw new AppError("E-mail e/ou senha incorreta", 401)
+		}
 
-    const { secret, expiresIn } = authConfig.jwt
+		const { secret, expiresIn } = authConfig.jwt
 
-    const token = sign({}, secret, {
-      subject: String(user.id),
-      expiresIn
-    })
+		const token = sign({}, secret, {
+			subject: String(user.id),
+			expiresIn
+		})
 
-    return response.json({ user, token })
-  }
+		return response.json({ user, token })
+	}
 }
