73a0d612d53f035624ca00717d7ee6f47f3fcab2
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-19 22:02:37 -0300

    feat: Add user token

--- src/controller/sessionsController.js
+++ src/controller/sessionsController.js
@@ -23,5 +23,14 @@ export class SessionsControllers {
     if (!passwordMatched) {
       throw new AppError("E-mail e/ou senha incorreta", 401)
     }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token })
   }
 }
