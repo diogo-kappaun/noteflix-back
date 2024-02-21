63c21860fab4e4902ceeea730ebfdadcbd2cdd5f
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-20 21:16:51 -0300

    feat: Add route avatar

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express"
 import { usersController } from "../controller/usersController.js"
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated.js"
+import multer from "multer"
+import uploadConfig from "../configs/upload.js"
+
+const upload = multer(uploadConfig.MULTER)
 
 const usersRoutes = Router()
 
@@ -9,5 +13,9 @@ const userController = new usersController()
 usersRoutes.post("/", userController.create)
 usersRoutes.put("/", ensureAuthenticated, userController.update)
 usersRoutes.delete("/:id", userController.delete)
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+	console.log(request.file.filename)
+	response.json()
+})
 
 export default usersRoutes
