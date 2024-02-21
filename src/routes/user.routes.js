df8e647be1190f2b5d271e8f99323bd7f5009aae
diogo-kappaun <diogohkappaun@gmail.com>
2024-02-20 21:47:51 -0300

    fix: Change the route function

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,19 +3,18 @@ import { usersController } from "../controller/usersController.js"
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated.js"
 import multer from "multer"
 import uploadConfig from "../configs/upload.js"
+import { UserAvatarController } from "../controller/userAvatarController.js"
 
 const upload = multer(uploadConfig.MULTER)
 
 const usersRoutes = Router()
 
 const userController = new usersController()
+const userAvatarController = new UserAvatarController()
 
 usersRoutes.post("/", userController.create)
 usersRoutes.put("/", ensureAuthenticated, userController.update)
 usersRoutes.delete("/:id", userController.delete)
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-	console.log(request.file.filename)
-	response.json()
-})
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 export default usersRoutes
