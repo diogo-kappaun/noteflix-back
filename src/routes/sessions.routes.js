95c84e7384f85431e491cb2299f0ff4d7bb1228f
diogo-kappaun <diogohkappaun@gmail.com>
2024-04-04 21:44:21 -0300

    feat: Add rate limit in login

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express"
+import { rateLimit } from "express-rate-limit"
 import { SessionsControllers } from "../controller/sessionsController.js"
+
+const loginRateLimiter = rateLimit({
+	windowMs: 1*60*1000,
+	max: 3,
+	standardHeaders: true,
+	legacyHeaders: false,
+	message: "Tentativas de login excedidas."
+})
 
 const sessionsControllers = new SessionsControllers()
 
 const sessionsRoutes = Router()
 
-sessionsRoutes.post("/", sessionsControllers.create)
+sessionsRoutes.post("/", loginRateLimiter, sessionsControllers.create)
 
 export default sessionsRoutes
