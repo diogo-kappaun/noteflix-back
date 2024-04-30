fe56864da2f49a46aed908c8c072ac29e7241d13
diogo-kappaun <diogohkappaun@gmail.com>
2024-04-29 22:21:36 -0300

    fix: limiter file route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
-import { Router } from "express"
+import { Router } from 'express'
 
-import moviesRoutes from "../routes/movies.routes.js"
-import tagsRoutes from "../routes/tags.routes.js"
-import usersRoutes from "../routes/user.routes.js"
-import { limiter } from "../utils/limiter.js"
-import sessionsRoutes from "./sessions.routes.js"
+import moviesRoutes from '../routes/movies.routes.js'
+import tagsRoutes from '../routes/tags.routes.js'
+import usersRoutes from '../routes/user.routes.js'
+import { limiter } from '../utils/Limiter.js'
+import sessionsRoutes from './sessions.routes.js'
 
 const routes = Router()
 
-routes.use("/users", limiter, usersRoutes)
-routes.use("/movies", moviesRoutes)
-routes.use("/tags", limiter, tagsRoutes)
-routes.use("/sessions", limiter, sessionsRoutes)
+routes.use('/users', limiter, usersRoutes)
+routes.use('/movies', moviesRoutes)
+routes.use('/tags', limiter, tagsRoutes)
+routes.use('/sessions', limiter, sessionsRoutes)
 
 export default routes
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,7 +1,7 @@
-import { Router } from "express"
-import { moviesController } from "../controller/moviesController.js"
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated.js"
-import { limiter } from "../utils/limiter.js"
+import { Router } from 'express'
+import { moviesController } from '../controller/moviesController.js'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js'
+import { limiter } from '../utils/Limiter.js'
 
 const moviesRoutes = Router()
 
@@ -9,9 +9,9 @@ const movieController = new moviesController()
 
 moviesRoutes.use(ensureAuthenticated)
 
-moviesRoutes.get("/", movieController.index)
-moviesRoutes.post("/", limiter, movieController.create)
-moviesRoutes.delete("/:id", limiter, movieController.delete)
-moviesRoutes.get("/:id", movieController.show)
+moviesRoutes.get('/', movieController.index)
+moviesRoutes.post('/', limiter, movieController.create)
+moviesRoutes.delete('/:id', limiter, movieController.delete)
+moviesRoutes.get('/:id', movieController.show)
 
 export default moviesRoutes
