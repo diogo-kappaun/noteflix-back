e7a8c690bbb9e665d2fed8c90627aa1015dfb83a
diogo-kappaun <diogohkappaun@gmail.com>
2024-04-29 22:21:55 -0300

    fix: Server port error

--- src/server.js
+++ src/server.js
@@ -1,37 +1,37 @@
-import cors from "cors"
-import "dotenv/config"
-import express from "express"
-import "express-async-errors"
-import uploadConfig from "./configs/upload.js"
-import { sqliteConnection } from "./database/sqlite/index.js"
-import routes from "./routes/index.js"
-import { AppError } from "./utils/AppError.js"
+import cors from 'cors'
+import 'dotenv/config'
+import express from 'express'
+import 'express-async-errors'
+import uploadConfig from './configs/upload.js'
+import { sqliteConnection } from './database/sqlite/index.js'
+import routes from './routes/index.js'
+import { AppError } from './utils/AppError.js'
 
 sqliteConnection()
 
 const app = express()
 app.use(express.json())
 
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(cors())
 app.use(routes)
 
 app.use((error, request, response, next) => {
-	if (error instanceof AppError) {
-		return response.status(error.statusCode).json({
-			status: "error",
-			message: error.message,
-		})
-	}
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
 
-	console.log(error)
+  console.log(error)
 
-	return response.status(500).json({
-		status: "error",
-		message: "Internal server error",
-	})
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
